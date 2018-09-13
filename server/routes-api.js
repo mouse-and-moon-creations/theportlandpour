83e39bc2d9a850a3f36c4235bd08b99a7e409a50
Tony M <tony@fbpllc.com>
2018-09-13 12:56:31 -0700

    replace server

---
+++ server/routes-api.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+router.get('/', (req, res, next) => {
+  res.json({});
+});
+
+export default router;
